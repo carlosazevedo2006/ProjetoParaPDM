42b6ad908c8e87eabae7db3413c600569d417fe1
carlosazevedo2006 <carlosalbertotininazevedo2006@gmail.com>
2025-11-17 14:17:38 +0000

    atualizacao de ficheiros

--- SnakeGame/babel.config.js
+++ SnakeGame/babel.config.js
@@ -1,24 +1,10 @@
-/**
- * CONFIGURAÇÃO DO BABEL PARA EXPO
- * 
- * O Babel é um transpiler que converte código moderno
- * para versões compatíveis com diferentes dispositivos
- */
 module.exports = function(api) {
-  // Cache da configuração para melhor performance
   api.cache(true);
-  
   return {
-    // Preset do Expo - inclui todas as configurações necessárias
     presets: ['babel-preset-expo'],
-    
-    // Plugins adicionais
     plugins: [
-      /**
-       * Plugin necessário para o react-native-gesture-handler
-       * Permite que os gestos funcionem corretamente
-       */
-      'react-native-reanimated/plugin',
+      // Remova esta linha se ainda causar problemas
+      // 'react-native-reanimated/plugin',
     ],
   };
 };
