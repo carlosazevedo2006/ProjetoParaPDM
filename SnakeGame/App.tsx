8e0cf0e8c63a7388b413638288f0ae73d505032f
carlosazevedo2006 <carlosalbertotininazevedo2006@gmail.com>
2025-11-18 10:59:07 +0000

    gt

--- SnakeGame/App.tsx
+++ SnakeGame/App.tsx
@@ -1,25 +1,20 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import HomeScreen from "./src/screens/HomeScreen";
-import GameScreen from "./src/screens/GameScreen";
-import GameOverScreen from "./src/screens/GameOverScreen";
-
-const Stack = createNativeStackNavigator();
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Game" component={GameScreen} />
-        <Stack.Screen name="GameOver" component={GameOverScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
